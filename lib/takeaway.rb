c521bc64ae13329c94a13fe627166bccf8cdf764
Sroop <sroopsunar@hotmail.com>
2014-04-25 15:47:53 +0100

    Temporary default order created to help calculate the total price of the order

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,7 +6,20 @@ class Takeaway
 
 	def menu
 		@menu = { "Salmon Sashimi" => 10, 
-			"Tuna Sashimni" => 7 }	
+			"Tuna Sashimi" => 7,
+			"Yellow Tail Sashimi" => 6,
+			"Mackerel Sashimi" => 8 }	
 	end
+
+	def receive_order
+		@order = { "Salmon Sashimi" => 10, 
+			"Tuna Sashimi" => 7,
+			"Mackerel Sashimi" => 8 }
+	end
+
+	def receipt
+		total_price = (receive_order.values).inject(0) { | accu, price | accu + price }
+	end
+
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,12 +4,38 @@ describe 'Takeaway' do
 	
 	let(:takeaway) { Takeaway.new }
 
-	it 'has a menu' do
-		expect(takeaway.menu)
+	context 'The Menu:' do
+
+		it 'exists when a takeaway is initialized' do
+			expect(takeaway.menu)
+		end
+
+		it 'has various dishes and prices in the menu' do
+			expect(takeaway.menu["Salmon Sashimi"]).to eq(10)
+			expect(takeaway.menu["Yellow Tail Sashimi"]).to eq(6)
+		end
+
 	end
 
-	it 'has various dishes and prices in the menu' do
-		expect(takeaway.menu["Salmon Sashimi"]).to eq(10)
+	context 'The Receipt:' do
+
+		it ' an order' do
+			expect(takeaway.receive_order).to eq({"Salmon Sashimi"=>10, "Tuna Sashimi"=>7, "Mackerel Sashimi"=>8})
+		end
+
+		it 'calculates the total price of an order' do
+			expect(takeaway.receive_order).to eq({"Salmon Sashimi"=>10, "Tuna Sashimi"=>7, "Mackerel Sashimi"=>8})
+			expect(takeaway.receipt).to eq(25)
+		end
+	
 	end
+
+	# context 'The Order:' do
+		
+	# 	it 'sends a text message when the order is placed' do
+			
+	# 	end
+
+	# end
 
 end
